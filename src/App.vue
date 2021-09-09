0effebc14f4399e7f6ca3831a1f80ca1b912996e
Rolf van der Toorn <rtoorn@compete.nl>
2021-09-09 15:02:10 +0200

    Test commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>Hello {{ value }}</div>
+  <div>
+    <Main />
+    <p>Some text here!</p>
+  </div>
 </template>
 
 <script>
+import Main from './components/Main.vue'
+
 export default {
   name: "App",
+  components: {
+    Main
+  },
   data() {
     return {
       value: "World"
